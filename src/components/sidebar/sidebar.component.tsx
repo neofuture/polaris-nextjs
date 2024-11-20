dd34cbeebd2c7dcc94bba6d21c4baa39b34641e7
Carl Fearby <carlfearby@me.com>
2024-11-20 14:21:11 +0000

    adding router

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -3,13 +3,17 @@
 import React from 'react';
 import {useAuth} from '@/context/AuthContext';
 import styles from './sidebar.module.css';
-import {useSidebar} from "@/context/SidebarContext";
+import {useSidebar} from '@/context/SidebarContext';
 import clsx from "clsx";
 import NavLink from "@/components/microcomponents/nav-link/nav-link.component";
+import {useRouter} from 'next/router';
 
 const Sidebar: React.FC = () => {
     const {isLoggedIn} = useAuth();
     const {isSidebarOpen} = useSidebar();
+    const router = useRouter();
+
+    const isActive = (href: string) => router.pathname.startsWith(href);
 
     return (
         <div className={clsx(styles.sidebar, {
@@ -23,14 +27,14 @@ const Sidebar: React.FC = () => {
                 <nav>
                     <ul className={styles['sidebar__nav-list']}>
                         <li className={styles['sidebar__nav-list-item']}>
-                            <NavLink iconName="fa-home" label="Home" href="/"/>
+                            <NavLink iconName="fa-home" label="Home" href="/" active={isActive("/")}/>
                         </li>
                         <li className={styles['sidebar__nav-list-item']}>
-                            <NavLink iconName="fa-dashboard" label="Dashboard" href="/dashboard"/>
+                            <NavLink iconName="fa-dashboard" label="Dashboard" href="/dashboard" active={isActive("/dashboard")}/>
                         </li>
                         {isLoggedIn && (
                             <li className={styles['sidebar__nav-list-item']}>
-                                <NavLink iconName="fa-user" label="Profile" href="/user/profile" active={true}/>
+                                <NavLink iconName="fa-user" label="Profile" href="/user/profile" active={isActive("/user/profile")}/>
                             </li>
                         )}
                     </ul>
