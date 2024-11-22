490672892154588f3e34977ecf3b218efca85d72
Carl Fearby <carlfearby@me.com>
2024-11-22 17:38:56 +0000

    Update layout.tsx

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import TopNavigation from "@/components/top-navigation/top-navigation.component";
 import Sidebar from "@/components/sidebar/sidebar.component";
-import CombinedProvider from "@/context/CombinedProvider";
 import styles from "./app.module.css";
 
 const projectName = process.env.NEXT_PUBLIC_PROJECT_NAME || "Default Project Name";
