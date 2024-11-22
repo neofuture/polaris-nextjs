01a3a9e4ff6f64cb6bea7f174d37a445ebd879c2
Carl Fearby <carlfearby@me.com>
2024-11-22 11:28:30 +0000

    Google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,13 @@ import TopNavigation from "@/components/top-navigation/top-navigation.component"
 import Sidebar from "@/components/sidebar/sidebar.component";
 import CombinedProvider from "@/context/CombinedProvider";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
 const projectName = process.env.NEXT_PUBLIC_PROJECT_NAME || "Default Project Name";
+const inter = Inter({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+});
 
 export const metadata = {
     title: projectName,
@@ -13,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" className={inter.className}>
         <head>
             <link rel="stylesheet" href="/css/fontawesome.min.css"/>
             <link rel="stylesheet" href="/css/duotone.min.css"/>
--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -2,8 +2,12 @@
 
 import React, {useEffect} from 'react';
 import withAuth from "@/hoc/with-auth";
+import NavLink from "@/components/microcomponents/nav-link/nav-link.component";
+import {useAuth} from "@/context/AuthContext";
 
-export default function Page() {
+function Page() {
+    const {logout} = useAuth();
+
     useEffect(() => {
         document.title = `${process.env.NEXT_PUBLIC_PROJECT_NAME} - Profile`;
     });
@@ -11,6 +15,9 @@ export default function Page() {
         <div>
             <h1><i className={'fad fa-user'}/> Profile</h1>
             <h2>Welcome to your profile</h2>
+            <NavLink iconName="fa-sign-out" onClick={logout}>Logout</NavLink>
         </div>
     );
 };
+
+export default withAuth(Page);
