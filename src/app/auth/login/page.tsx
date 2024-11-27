9be0aadc98026fe0bc0a5f8afe056c5ea48d5edc
Carl Fearby <carlfearby@me.com>
2024-11-27 22:55:45 +0000

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-import React, {useEffect} from 'react';
-import {useAuth} from "@/context/AuthContext";
+import React, { useEffect } from 'react';
+import { useAuth } from "@/context/AuthContext";
 import Button from "@/components/microcomponents/button/button.component";
-import {redirect} from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 const Login: React.FC = () => {
-    const {isLoggedIn, login} = useAuth();
+    const { isLoggedIn, login } = useAuth();
+    const router = useRouter();
+
     useEffect(() => {
         document.title = `${process.env.NEXT_PUBLIC_PROJECT_NAME} - Login`;
-    });
+    }, []);
+
     useEffect(() => {
         if (isLoggedIn) {
-            redirect('/app');
+            router.push('/app');
         }
-    }, [isLoggedIn]);
+    }, [isLoggedIn, router]);
 
     return (
         <div>
-            <h1><i className={'fad fa-sign-in'}/> Login</h1>
+            <h1><i className={'fad fa-sign-in'} /> Login</h1>
             <h2>Welcome to the login</h2>
             <Button onClick={login} iconName={'fa-sign-in'}>Login</Button>
         </div>
