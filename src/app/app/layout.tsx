beb99b544c5bf28161310eb0829c7897935d12f9
Carl Fearby <carlfearby@me.com>
2024-12-01 12:30:27 +0000

    fixing errors

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import TopNavigation from "@/components/top-navigation/top-navigation.component";
 import Sidebar from "@/components/sidebar/sidebar.component";
 import styles from "./app.module.css";
-import ToastManager from "@/components/microcomponents/toast/toast-manager.component";
 
 const projectName = process.env.NEXT_PUBLIC_PROJECT_NAME || "Default Project Name";
 
